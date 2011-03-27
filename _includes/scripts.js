2f67fb47d99e329c3dbd86ee376bb60bb3a724fa
Konrad Malawski <konrad.malawski@project13.pl>
2011-03-27 14:45:27 +0200

    added loading of github projects :-)

--- _includes/scripts.js
+++ _includes/scripts.js
@@ -4,4 +4,30 @@ $(document).ready(function() {
 
     $("#my-cv").mailto();
     $("#my-cv").autolink();
+
+    $('#show-pdf').click(function() {
+        var pdfUrl = 'https://github.com/ktoso/cv/raw/master/konrad_malawski_cv.pdf';
+        $('#pdf-container').html('<iframe src="http://docs.google.com/gview?url=' + pdfUrl + '&embedded=true" style="width:100%; height:700px;" frameborder="0"></iframe>');
+    });
+
+    getAllRepos('ktoso');
 });
+
+
+function getAllRepos(username) {
+    var url = 'http://github.com/api/v2/json/repos/show/' + username + '?callback=?';
+    var repoTmpl = "repo";
+    $.template(repoTmpl, "<li><a href=\"${url}\" title=\"${title}\">${name}</a></li>");
+
+    $.getJSON(url, function(data, textStatus) {
+        console.log('got data');
+
+        var container = $('#other-projects');
+        var list = $('<ul></ul>');
+        container.append(list);
+
+        $.each(data.repositories, function(index, repo) {
+            $.tmpl(repoTmpl, repo).appendTo(list);
+        });
+    });
+}
