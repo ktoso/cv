70eeaef1b7f49f1d2e25fc3ce1b90a527b6df0b8
Konrad Malawski <konrad.malawski@project13.pl>
2011-03-27 15:12:47 +0200

    implemented "repo ticker" :-)

--- _includes/scripts.js
+++ _includes/scripts.js
@@ -1,23 +1,40 @@
-$(document).ready(function() {
-
-    $("#contact-me").mailto();
-
-    $("#my-cv").mailto();
-    $("#my-cv").autolink();
-
+function setupShowPdf() {
     $('#show-pdf').click(function() {
+        // todo optimize
+        $(this).hide();
+        $('#hide-pdf').show();
         var pdfUrl = 'https://github.com/ktoso/cv/raw/master/konrad_malawski_cv.pdf';
-        $('#pdf-container').html('<iframe src="http://docs.google.com/gview?url=' + pdfUrl + '&embedded=true" style="width:100%; height:700px;" frameborder="0"></iframe>');
+
+        var pdf = $('#pdf-container');
+        pdf.css('display', 'none')
+                .html('<iframe src="http://docs.google.com/gview?url=' + pdfUrl + '&embedded=true" style="width:100%; height:700px;" frameborder="0"></iframe>')
+                .slideDown();
+
     });
+}
+function setupHidePdf() {
+    $('#hide-pdf').click(function() {
+        // todo optimize
+        $(this).hide();
+        $('#show-pdf').show();
+        $('#pdf-container').hide();
+    });
+}
 
-    getAllRepos('ktoso');
-});
+function startRepoTicker() {
+    $("#other-projects").jCarouselLite({
+        vertical: true,
+        hoverPause:true,
+        visible: 1,
+        auto:500,
+        speed:1000
+    });
+}
 
-
-function getAllRepos(username) {
+function initRepoTicker(username) {
     var url = 'http://github.com/api/v2/json/repos/show/' + username + '?callback=?';
     var repoTmpl = "repo";
-    $.template(repoTmpl, "<li><a href=\"${url}\" title=\"${title}\">${name}</a></li>");
+    $.template(repoTmpl, '<li><a href="${url}" title="${title}">${name}</a></li>');
 
     $.getJSON(url, function(data, textStatus) {
         console.log('got data');
@@ -29,5 +46,22 @@ function getAllRepos(username) {
         $.each(data.repositories, function(index, repo) {
             $.tmpl(repoTmpl, repo).appendTo(list);
         });
+
+        startRepoTicker();
     });
 }
+
+//-----------------------------------------------------------------------------
+
+$(document).ready(function() {
+
+    $("#contact-me").mailto();
+
+    $("#my-cv").mailto();
+    $("#my-cv").autolink();
+
+    setupHidePdf();
+    setupShowPdf();
+
+    initRepoTicker('ktoso');
+});
